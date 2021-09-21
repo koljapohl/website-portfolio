7ec5874f156e6d5185edf0dc245e33e89ee17786
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-09-21 10:10:39 +0200

    [ADD] Modal structure

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,17 +1,17 @@
 //enabling input validation for contact form
-(function() {
+(function formValidation () {
     let form = document.querySelector('#contact-form');
     let emailInput = form.querySelector('#mail');
     let msgInput = form.querySelector('#msgbox');
 
-    function validateEmail() {
+    function validateEmail () {
         let value = emailInput.value;
 
-        if(!value) {
+        if (!value) {
             showErrorMessage(emailInput, 'E-mail is a required field.');
             return false;
         }
-        if(value.indexOf('@') === -1) {
+        if (value.indexOf('@') === -1) {
             showErrorMessage(emailInput, 'You must enter a valid email address.');
             return false;
         }
@@ -19,9 +19,9 @@
         return true;
     }
 
-    function validateMsg() {
+    function validateMsg () {
         let value = msgInput.value;
-        if(!value || value.length <= 10) {
+        if (!value || value.length <= 10) {
             showErrorMessage(msgInput, 'Please enter a message. (At least 10 characters)');
             return false;
         }
@@ -29,15 +29,15 @@
         return true;
     }
 
-    function showErrorMessage(input, message) {
+    function showErrorMessage (input, message) {
         let wrapper = input.parentElement;
 
         let error = wrapper.querySelector('.error-message');
-        if(error) {
+        if (error) {
             wrapper.removeChild(error);
         }
 
-        if(message) {
+        if (message) {
             let error = document.createElement('div');
             error.classList.add('error-message');
             error.innerText = message;
---
+++ js/screenshot.js
@@ -0,0 +1,7 @@
+function showScreenshot () {
+  let modalBody = document.querySelector('.modal-body');
+  modalBody.setAttribute('style', 'display: grid;');
+  console.log('click fired.');
+
+
+};
